217177fc9d3b8b61c48422ad1304135f66d55a68
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2023-11-17 07:39:44 -0600

    Draft of MP8.

---
+++ code/mps/image-series.rkt
@@ -0,0 +1,71 @@
+#lang racket
+
+(require csc151)
+(require 2htdp/image)
+(require rackunit)
+
+;;; image-series.rkt
+;;;   The final project for CSC-151-01 2023Fa
+;;;
+;;; Authors:
+;;;   Person One
+;;;   Person Two
+;;;   Person Three
+;;;
+;;; Date: YYYY-MM-DD
+;;;
+;;; Acknowledgements
+;;; 
+;;; * The project template comes from SamR.
+
+; +----------+-------------------------------------------------------
+; | Time log |
+; +----------+
+
+#|
+NAME    YYYY-MM-DD      HH:MM-HH:MM     ACTIVITY
+NAME    YYYY-MM-DD      HH:MM-HH:MM     ACTIVITY
+NAME    YYYY-MM-DD      HH:MM-HH:MM     ACTIVITY
+NAME    YYYY-MM-DD      HH:MM-HH:MM     ACTIVITY
+NAME    YYYY-MM-DD      HH:MM-HH:MM     ACTIVITY
+NAME    YYYY-MM-DD      HH:MM-HH:MM     ACTIVITY
+NAME    YYYY-MM-DD      HH:MM-HH:MM     ACTIVITY
+
+|#
+
+; +--------------+---------------------------------------------------
+; | Design goals |
+; +--------------+
+
+#|
+
+
+|#
+
+; +------------------------+-----------------------------------------
+; | Requirements checklist |
+; +------------------------+
+
+#|
+
+
+|#
+
+; +-------------------+----------------------------------------------
+; | Primary procedure |
+; +-------------------+
+
+;;; (image-series n width height) -> image?
+;;;   n : non-negative-integer?
+;;;   width : non-negative-integer?
+;;;   height : non-negative-integer?
+;;; Create an image of the given width and height that meets the design
+;;; goals specified above.  Given the same `n`, always creates the same 
+;;; image.  Given different values of `n`, creates different images.
+(define image-series
+  (lambda (n)
+    ???))
+
+; +-------------------+----------------------------------------------
+; | Helper procedures |
+; +-------------------+
